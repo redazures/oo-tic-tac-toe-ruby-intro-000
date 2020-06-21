ec11bba9b244b78ed095d6741f6f438ae4b8a524
james wu <jwu_nyc@yahoo.com>
2020-06-21 14:46:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,6 @@
 class TicTacToe
 
+<<<<<<< HEAD
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
@@ -12,6 +13,16 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
 
+=======
+  def initialize (board = nil)
+    @board = board || Array.new(9," ")
+  end
+
+  def board
+    @board
+  end
+  #This is the display function
+>>>>>>> 0bda5da572dd7e0426fc04a6c427ed4a6f6b53b4
   def display_board # This is how we create a board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -20,21 +31,43 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+<<<<<<< HEAD
   def input_to_index2   #This will strings into numbers which is useful for inputs that come from the user
+=======
+  #This will strings into numbers which is useful for inputs that come from the user
+  def input_to_index
+>>>>>>> 0bda5da572dd7e0426fc04a6c427ed4a6f6b53b4
     input=gets.strip
     input.to_i-1
   end
 
+<<<<<<< HEAD
   def input_to_index(user_input)
   user_input.to_i-1
   end
 
   #This uses the current player to put on a board that we have created. We still need to supply board and index
   def move(index, current_player)
+=======
+  #This is an assist function that will help with counting what turn is it
+  def turn_count
+    @board.count{|token| token=="X"||token=="O"}
+  end
+
+  #This will determine which player is setting the letter on the board
+  #Curre_player function uses the turn_count method to determine which player is playing
+  def current_player
+    turn_coun@board %2==0? "X":"O"
+  end
+
+  #This uses the current player to put on a board that we have created. We still need to supply board and index
+  def move(board, index,current_player)
+>>>>>>> 0bda5da572dd7e0426fc04a6c427ed4a6f6b53b4
     @board[index] = current_player
   end
 
   #This function will access if the position on the board is empty or not
+<<<<<<< HEAD
   def position_taken? (index)
     @board[index] != " " && @board[index] !=""
   end
@@ -127,3 +160,53 @@ class TicTacToe
   end
 
 end
+=======
+  def position_taken?(location)
+    @board[location] != " " && @board[location] !=""
+  end
+
+  #This function will use the position_taken function is determine if the player is setting down on an "empty slot"
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board,index)
+  end
+
+  #All functions are use somewhat to complete this function. We also have it looped itself to get the inputs again
+  def turn
+    puts "Please enter 1-9:"
+    index=input_to_index
+    while valid_move?(index)==false
+      puts "This (#{index}) is not a valid move. Please input correct move"
+      input=gets.strip
+      index=input_to_index
+    end
+    move(index,current_player)
+    display_board
+    #puts 1+converted_number
+  end
+
+  #This line divides the two parts of this game. The above the dynamics of motion for this game
+end
+
+class Person
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+
+end
+
+james=Person.new("james", "wu")
+james.name=("jimmy")
+puts james.name
+>>>>>>> 0bda5da572dd7e0426fc04a6c427ed4a6f6b53b4
